bfc228f685fd6c774ca0888a66653d4e53565aff
Jonathan Sánchez <jonulisanpar@gmail.com>
2024-04-22 17:03:27 -0600

    Initial commit

---
+++ src/main/java/com/jonathan/curso/springboot/app/aop/springbootaop/SpringbootAopApplication.java
@@ -0,0 +1,15 @@
+package com.jonathan.curso.springboot.app.aop.springbootaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+// @EnableAspectJAutoProxy // Para nuevas versiones no es necesario
+@SpringBootApplication
+public class SpringbootAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jonathan/curso/springboot/app/aop/springbootaop/controllers/GreetingController.java
@@ -0,0 +1,27 @@
+package com.jonathan.curso.springboot.app.aop.springbootaop.controllers;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jonathan.curso.springboot.app.aop.springbootaop.services.GreetingService;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private GreetingService greeting;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<?> greeting() {
+        return ResponseEntity.ok(Collections.singletonMap("greetings", greeting.sayHello("Pepe", "Hola que tal!")));
+    }
+
+    @GetMapping("/greeting-error")
+    public ResponseEntity<?> greetingError() {
+        return ResponseEntity.ok(Collections.singletonMap("greetings", greeting.sayHelloError("Pepe", "Hola que tal!")));
+    }
+}
